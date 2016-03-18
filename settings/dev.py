ef7f6acd307d7c63dce9c7e16283c78edff55c33
wanghe <wanghe@loginsight.cn>
2016-03-18 16:01:50 +0800

    posgresql

--- settings/dev.py
+++ settings/dev.py
@@ -1,8 +1,19 @@
 from .base import *
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'example.sqlite',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'example.sqlite',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sentry.db',
+        'USER': 'wanghe',
+        'PASSWORD': 'yuxuangh',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
